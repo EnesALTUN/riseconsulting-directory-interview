0e1c13cd489b936a63ffadd8e4abcb87552f24c1
Enes ALTUN <ensalt_1998@hotmail.com>
2021-11-26 22:05:35 +0300

    added ocelot configuration in apigateway

--- RiseConsulting.Directory.ApiGateway/Program.cs
+++ RiseConsulting.Directory.ApiGateway/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RiseConsulting.Directory.ApiGateway
 {
@@ -18,6 +13,10 @@ namespace RiseConsulting.Directory.ApiGateway
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hosting, config) =>
+                {
+                    config.AddJsonFile("ocelot.json", false, true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();

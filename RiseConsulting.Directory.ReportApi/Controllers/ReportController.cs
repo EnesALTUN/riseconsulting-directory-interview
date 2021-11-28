73937a379d03c53b873604cc511cff833704d1f4
Enes ALTUN <ensalt_1998@hotmail.com>
2021-11-28 16:06:35 +0300

    added report api

---
+++ RiseConsulting.Directory.ReportApi/Controllers/ReportController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RiseConsulting.Directory.ReportApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
